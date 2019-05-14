79eea5d863c9d9b625aea5a9486cf40c11407f3c
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 01:40:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -11,20 +11,25 @@ function getRepositories() {
 
 }
 
-function displayCommits() {
-  const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits
-    .map(
-      commit =>
-        '<li><h3>' +
-        commit.commit.author.name +
-        ' (' +
-        commit.author.login +
-        ')</h3>' +
-        commit.commit.message +
-        '</li>'
-    ).join('')}</ul>`;
-  document.getElementById('details').innerHTML = commitsList;
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoList =
+    '<ul>' +
+    repos
+      .map(repo => {
+        const dataUsername = 'data-username="' + repo.owner.login + '"';
+        const dataRepoName = 'data-repository="' + repo.name + '"';
+        return `
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`;
+      })
+      .join('') +
+    '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
 
 function getCommits(el) {
