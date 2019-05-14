df70af0cf588e80a17e12dd196aed2787cb422f4
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 00:44:39 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -21,5 +21,5 @@ function displayRepositories() {
   // `<ul>${repos.map(r => '<li>' +
   //  r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
 
-  document.getElementById('repositories').innerHTML = repoList;
+  // document.getElementById('repositories').innerHTML = repoList;
 }
