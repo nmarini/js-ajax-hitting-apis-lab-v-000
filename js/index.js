6096944f0851375aa177e2712d393de03ba525a5
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 01:41:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ const rootURL = 'https://api.github.com';
 function getRepositories() {
   const name = document.getElementById('username').value;
   const uri = rootURL + '/users/' + name + '/repos';
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', displayRepositories);
-  xhr.open('GET', uri);
-  xhr.send();
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', uri);
+  req.send();
 
 }
 
