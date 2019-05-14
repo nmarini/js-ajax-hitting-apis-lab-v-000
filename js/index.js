f79dfd3aca29ec1cfe937499832dcb0a30ade19a
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 00:57:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -13,14 +13,23 @@ function getRepositories() {
 
 function displayRepositories() {
   const repos = JSON.parse(this.responseText);
-  console.log(repos)
-  debugger
-  // const repoList = '<ul>' +
-  //   repos.map(repo => {
-  //     const
-  //   })
-  // `<ul>${repos.map(r => '<li>' +
-  //  r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
-
-  // document.getElementById('repositories').innerHTML = repoList;
+  const repoList = '<ul>' +
+    repos.map(repo => {
+      const repoList =
+    '<ul>' +
+    repos
+      .map(repo => {
+        const dataUsername = 'data-username="' + repo.owner.login + '"';
+        const dataRepoName = 'data-repository="' + repo.name + '"';
+        return `
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`;
+      })
+      .join('') +
+    '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
 }
