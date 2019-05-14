460f8961a6ae48f5c0f9b33de6186f2422123f00
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 01:23:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,44 +1,34 @@
 const rootURL = 'https://api.github.com';
 
-// function getRepositories() {
-//   const name = document.getElementById('username').value;
-//   const req = new XMLHttpRequest();
-//   const uri = rootURL + '/users/' + name + '/repos'
-//
-//   req.addEventListener('load', displayRepositories())
-//   req.open('GET', uri);
-//   req.send();
-//
-// }
 function getRepositories() {
   const name = document.getElementById('username').value;
-  const uri = rootURL + '/users/' + name + '/repos';
-  const xhr = new XMLHttpRequest();
-  xhr.addEventListener('load', displayRepositories);
-  xhr.open('GET', uri);
-  xhr.send();
-  return false;
+  const req = new XMLHttpRequest();
+  const uri = rootURL + '/users/' + name + '/repos'
+
+  req.addEventListener('load', displayRepositories())
+  req.open('GET', uri);
+  req.send();
+
 }
-//
 
 function displayRepositories() {
   const repos = JSON.parse(this.responseText);
-  const repoList = '<ul>' +
-    repos.map(repo => {
-      const repoList =
-        '<ul>' +
-        repos.map(repo => {
-          const dataUsername = 'data-username="' + repo.owner.login + '"';
-          const dataRepoName = 'data-repository="' + repo.name + '"';
-          return `
-            <li>
-              <h2>${repo.name}</h2>
-              <a href="${repo.html_url}">${repo.html_url}</a><br>
-              <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-              <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
-            </li>`;
-        }).join('') +
-        '</ul>';
+  const repoList =
+    '<ul>' +
+    repos
+      .map(repo => {
+        const dataUsername = 'data-username="' + repo.owner.login + '"';
+        const dataRepoName = 'data-repository="' + repo.name + '"';
+        return `
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`;
+      })
+      .join('') +
+    '</ul>';
   document.getElementById('repositories').innerHTML = repoList;
 }
 
