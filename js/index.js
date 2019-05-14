1b2be2efd98b7216b86497e19ddb699eb506dbe2
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 01:33:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -69,7 +69,7 @@ function getBranches(el) {
 function displayBranches() {
   const branches = JSON.parse(this.responseText);
   const branchesList = '<ul>' +
-    branches.map(branch => 
+    branches.map(branch =>
       '<li>' +
       branch.name +
       '</li>'
