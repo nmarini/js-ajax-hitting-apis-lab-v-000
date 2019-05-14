41df918d9575e90722a0c36d2e486feba31108b7
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 00:04:28 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// your code here
+const rootURL = 'https://api.github.com';
+
+function getRepositories() {
+  const name = document.getElementById('username').value;
+  const req = new XMLHttpRequest();
+  const uri = rootURL + '/users/' + name + '/repos'
+
+  req.addEventListener('load', displayRepositories())
+  req.open('GET', uri);
+  req.send();
+
+}
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos.map(r => '<li>' +
+   r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
+
+  document.getElementById('repositories').innerHTML = repoList;
+}
