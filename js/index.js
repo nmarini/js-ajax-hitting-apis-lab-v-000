d8b93f4d187c5c853431b276c90aea26f0b3bd54
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 00:41:09 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -13,8 +13,13 @@ function getRepositories() {
 
 function displayRepositories() {
   const repos = JSON.parse(this.responseText);
-  const repoList = `<ul>${repos.map(r => '<li>' +
-   r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
+  console.log(repos)
+  const repoList = '<ul>' +
+    repos.map(repo => {
+      const 
+    })
+  // `<ul>${repos.map(r => '<li>' +
+  //  r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
 
   document.getElementById('repositories').innerHTML = repoList;
 }
