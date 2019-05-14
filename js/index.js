3767b9b736a2a959863da9c80279f64edd1a6fcf
Nicolas Marini <nmarini44@gmail.com>
2019-05-14 01:32:29 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -44,13 +44,15 @@ function getCommits(el) {
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
   const commitsList = '<ul>' +
-    commits.map(commit =>
-      '<li><strong>' +
-      commit.author.login +
-      '</strong> - ' +
-      commit.commit.message +
-      '</li>'
-    ).join('') +
+    commits.map(commit => {
+      '<li><h3>' +
+       commit.commit.author.name +
+       ' (' +
+       commit.author.login +
+       ')</h3>' +
+       commit.commit.message +
+       '</li>'
+    }).join('') +
     '</ul>';
   document.getElementById('details').innerHTML = commitsList;
 }
